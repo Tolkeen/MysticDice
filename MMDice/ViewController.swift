75024d570b7d9e87df5a214f53a40b952ab0d76e
Joshua Rodriguez <rvismund@gmail.com>
2015-08-23 13:11:29 -0500

    changes

--- MMDice/ViewController.swift
+++ MMDice/ViewController.swift
@@ -58,6 +58,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
+        self.view.backgroundColor = UIColor.grayColor()
     }
 
     override func didReceiveMemoryWarning() {
